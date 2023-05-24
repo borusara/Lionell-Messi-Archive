b1abb48126216db9c0f7c8c1fd41a453152a730d
borusara <125817831+borusara@users.noreply.github.com>
2023-05-24 12:06:39 +0530

    Update alive.py

--- Messi/modules/alive.py
+++ Messi/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/9b2f5c511b0ecdfbabdcf.mp4"
 
 @register(pattern=("awake messi"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lionell Messi** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Shoushuke Komi** \n\n"
   TEXT += "🗡 **I'm Working Properly** \n\n"
-  TEXT += f"🗡 **My Masters : [Messi Probot Team](https://t.me/Messi_Probot_Team)** \n\n"
+  TEXT += f"🗡 **My Masters : [Shoushuke creators](https://t.me/Knights_X_association/34)** \n\n"
   TEXT += f"🗡 **Library Version :** `{x}` \n\n"
   TEXT += f"🗡 **Telethon Version :** `{y}` \n\n"
   TEXT += f"🗡 **Pyrogram Version :** `{z}` \n\n"
   TEXT += "**⚽️ Thanks For Adding Me Here ⚽️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Messi_Probot?start=help"), Button.url("My Headquarters", "https://t.me/Messi_Probot_SUpport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Messi_Probot?start=help"), Button.url("My Headquarters", "https://t.me/shoushuke_support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
