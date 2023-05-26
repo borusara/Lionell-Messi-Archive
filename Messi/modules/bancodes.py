6bd4c891e929aef586928ee635af42b2749b2d89
PrimexRick <118347352+PrimexRick@users.noreply.github.com>
2023-05-26 18:48:50 +0530

    Update bancodes.py

--- Messi/modules/bancodes.py
+++ Messi/modules/bancodes.py
@@ -6,20 +6,20 @@ from Messi.events import register
 from Messi import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/9b2f5c511b0ecdfbabdcf.mp4"
+PHOTO = "https://te.legra.ph/file/48d33c9cba111e5938fda.jpg"
 
 @register(pattern=("bancodes"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lionell Messi** \n\n"
-  TEXT += "🗡 **Here are bancodes for [Messi](https://t.me/messi_probot)** \n\n"
-  TEXT += f"🗡 **My Masters : [Messi Probot Team](https://t.me/Messi_Probot_Team)** \n\n"
-  TEXT += f"🗡 **Library Version :** `{x}` \n\n"
-  TEXT += f"🗡 **Library Version :** `{x}` \n\n"
-  TEXT += f"🗡 **Library Version :** `{x}` \n\n"
-  TEXT += f"🗡 **Library Version :** `{x}` \n\n"
-  TEXT += f"🗡 **Library Version :** `{x}` \n\n"
-  TEXT += f"🗡 **Telethon Version :** `{y}` \n\n"
-  TEXT += f"🗡 **Pyrogram Version :** `{z}` \n\n"
-  TEXT += "**⚽️ Thanks For Starting Me ⚽️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Messi_Probot?start=help"), Button.url("My Headquarters", "https://t.me/Messi_Probot_SUpport")]]
+  TEXT += "🗡 **Here are bancodes for [Shoushuke](https://t.me/shoushuke_komi_bot)** \n\n"
+  TEXT += f"🗡 **My Masters : [Primes Team](https://t.me/PrimesDivision)** \n\n"
+  TEXT += f"🗡 **Abuse**  \n\n"
+  TEXT += f"🗡 **Mass Add**  \n\n"
+  TEXT += f"🗡 **Spambot**  \n\n"
+  TEXT += f"🗡 **Raid Initiator**  \n\n"
+  TEXT += f"🗡 **Raid Participant**  \n\n"
+  TEXT += f"🗡 **NSFW spammer** \n\n"
+  TEXT += f"🗡 **Refer to Slayer Bancodes for more info**  \n\n"
+  TEXT += "**🔥 Thanks For Starting Me 🔥**"
+  BUTTON = [[Button.url("Help", "https://t.me/shkushuke_komi_bot?start=help"), Button.url("My Headquarters", "https://t.me/Shoushuke_Support")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
